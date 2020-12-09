9d97c6c4d83a1b89f723e9e33ab29059bc330a78
Mariano Zeppa <zeppamariano@gmail.com>
2020-12-09 13:00:10 +1300

    minesweeper

---
+++ minesweeper.js
@@ -0,0 +1,82 @@
+document.addEventListener('DOMContentLoaded', startGame)
+
+// Define your `board` object here!
+var board = {
+  cells: [
+    {row: 1,
+     col: 1,
+     isMine: false,
+     hidden: true},
+    {row: 1,
+     col: 2,
+     isMine: false,
+   hidden: true},
+    {row: 1,
+     col: 3,
+     isMine: true,
+   hidden: true},
+    {row: 2,
+     col: 1,
+     isMine: true,
+   hidden: true},
+     {row: 2,
+      col: 2,
+      isMine: true,
+    hidden: true},
+     {row: 2,
+      col: 3,
+      isMine: true,
+    hidden: true},
+     {row: 3,
+      col: 1,
+      isMine: true,
+    hidden: true},
+     {row: 3,
+      col: 2,
+      isMine: true,
+    hidden: true},
+      {row: 3,
+       col: 3,
+       isMine: false,
+     hidden: true}]
+}
+function startGame () {
+for (i = 0; i < board.cells.length; i++){
+  board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+
+}
+  // Don't remove this function call: it makes the game work!
+  lib.initBoard()
+}
+
+// Define this function to look for a win condition:
+//
+// 1. Are all of the cells that are NOT mines visible?
+// 2. Are all of the mines marked?
+function checkForWin () {
+
+  // You can use this function call to declare a winner (once you've
+  // detected that they've won, that is!)
+  //   lib.displayMessage('You win!')
+}
+
+// Define this function to count the number of mines around the cell
+// (there could be as many as 8). You don't have to get the surrounding
+// cells yourself! Just use `lib.getSurroundingCells`:
+//
+  //var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+  //console.log(surroundings);
+//
+// It will return cell objects in an array. You should loop through
+// them, counting the number of times `cell.isMine` is true.
+function countSurroundingMines (cell) {
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+  var count = 0;
+  for (i = 0; i < surrounding.length; i++){
+      if (surrounding[i].isMine == true){
+      count ++;
+    }
+
+   }
+    return count;
+}
